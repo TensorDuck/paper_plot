04f105e746a93fa938a71e6c01ad1338f8fa2d4b
Justin Chen <jchen978@gmail.com>
2020-04-28 20:28:17 -0700

    Update example script

--- examples/plot_lines.py
+++ examples/plot_lines.py
@@ -1,4 +1,15 @@
+import numpy as np
+
 from paper_plot import Plotter
 
+
+def get_dummy_data():
+    x = np.arange(100)
+    y = np.random.random(len(x))
+    return x, y
+
+
 if __name__ == "__main__":
-    plotter = Plotter("data", save_dir_name="test")
+    x, y = get_dummy_data
+    plotter = Plotter(save_dir="data")
+    plotter.plot_line(x, y)
